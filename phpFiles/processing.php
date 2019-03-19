efdd0b0de09ac544136f67ece232663d7bff3a88
Terry-James <37746020+Terry-James@users.noreply.github.com>
2019-03-19 06:28:38 -0500

    added insert to php file

--- phpFiles/processing.php
+++ phpFiles/processing.php
@@ -2,11 +2,11 @@
 $db_host = 'localhost';
 $db_username = 'root';
 $db_pass = '';
-$db_name = '';
+$db_name = 'carstore';
 
 $db = new mysqli($db_host, $db_username, $db_pass, $db_name) or die("Can't connect to MySQL Server");
 
-
-
+$addingUser = ("INSERT INTO () VALUES ('')");
+$addingQuery = mysqli_query($db, $addingUser);
 
 ?>
