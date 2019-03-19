ac1befe8586d6da601812e3d9f729962e60f5842
Terry-James <37746020+Terry-James@users.noreply.github.com>
2019-03-18 20:25:38 -0500

    added color schema for css a login page, registration, and a main store front page. Also added php script for processing user data

---
+++ phpFiles/processing.php
@@ -0,0 +1,12 @@
+<?php
+$db_host = 'localhost';
+$db_username = 'root';
+$db_pass = '';
+$db_name = '';
+
+$db = new mysqli($db_host, $db_username, $db_pass, $db_name) or die("Can't connect to MySQL Server");
+
+
+
+
+?>
