161a47587fd94c8d67660ba0523bb133f86e8d07
Terry-James <37746020+Terry-James@users.noreply.github.com>
2019-03-24 16:28:25 -0500

    finished login and registration functions for moving to next page and inserting to the database

--- phpFiles/processing.php
+++ phpFiles/processing.php
@@ -5,37 +5,43 @@ $db_username = 'root';
 $db_pass = '';
 $db_name = 'carstore';
 
-$HiddentInput = $_POST['hiddentData'];
-if ($HiddentInput=="LogIn")
-    HandleLogInFunction($_POST);
-else
-    HandleRegisterationFunction($_POST);
-// user inputs from html page
-$email = $_POST['email'];
-$firstName = $_POST['firstName'];
-$lastName = $_POST['lastName'];
-$password = $_POST['password'];
-
-$userEmail = $_POST['userEmail'];
-$userPassword = $_POST['userPassword'];
-
-
-// connect to the database
 $db = new mysqli($db_host, $db_username, $db_pass, $db_name) or die("Can't connect to MySQL Server");
 
-// insert query
-if(isset($firstName)){
+
+$HiddentInput = $_POST['hiddentData'];
+
+if ($HiddentInput=="LogIn"){
+    $userEmail = $_POST['userEmail'];
+    $userPassword = $_POST['userPassword'];
+    HandleLogInFunction($db, $userEmail, $userPassword);
+}  
+else{
+    $email = $_POST['email'];
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $password = $_POST['password'];
+    HandleRegisterationFunction($db, $email, $firstName, $lastName, $password);
+}
+
+function HandleRegisterationFunction($email, $firstName, $lastName, $password){
+    
     $addingUser = ("INSERT INTO customers(email,firstName,lastName,password) VALUES ('$email','$firstName','$lastName', '$password')");
     $addingQuery = mysqli_query($db, $addingUser);
+    header('Location:index.html');
 }
-elseif(isset($userEmail)){
-    $getCustomerInfo = ("SELECT email, password From customers  Where 'email' = '$userEmail'");
-    $getInfoQuery = mysqli_query($db, $getCustomerInfo);
-    $customerInfo = mysqli_fetch_assoc($getInfoQuery);
 
-    if($customerInfo['email'] == '$userEmail'){
+
+function HandleLogInFunction($db, $userEmail, $userPassword){
     
+    $getCustomerInfo = ("SELECT email, password From customers  Where email = '$userEmail'");
+    $getInfoQuery = mysqli_query($db, $getCustomerInfo);
+    $customerInfo = mysqli_fetch_array($getInfoQuery);
+
+    if($customerInfo['email'] === $userEmail && $customerInfo['password'] === $userPassword){
+        header('Location:/OnlineCarStore/store.html');
+    }
+    else{
+        header('Location:/OnlineCarStore/index.html');
     }
 }
-
 ?>
