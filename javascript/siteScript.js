735f153c152ab99124f3fc22b230d30700379d30
Omar <omarcollado51@gmail.com>
2019-03-24 18:07:50 -0600

    Added a navigation menu to index.html, added some javascript to hide and show login, and updated colors and CSS in siteStyle.css

--- javascript/siteScript.js
+++ javascript/siteScript.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
     $("#logOut").click(returnHome);
+    $("#navLogin").click(openLogin);
 });
 
 function returnHome(){
     location = "index.html";
+}
+
+function openLogin() {
+  $(".row")[0].className = "row";
 }
