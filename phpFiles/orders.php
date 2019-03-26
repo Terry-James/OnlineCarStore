c95d91fea7c5714b8131a2159fecc037545bc048
OmarCollado <omarcollado51@gmail.com>
2019-03-25 18:25:46 -0700

    Created orders.php

---
+++ phpFiles/orders.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Store Front</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../cssFiles/siteStyle.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="../javascript/navScript.js"></script>
+    <script src="../javascript/storeScript.js"></script>
+</head>
+
+<body>
+    <div class="mainNav">
+        <ul>
+            <li><button id="logOut">log-out</button></li>
+            <li><button id="viewCars"> View Cars </button></li>
+            <li><button id="viewOrders"> View Orders</button></li>
+            <form id="searchBar">
+                <input type="text" placeholder="Search" name="search">
+                <button type="submit">Submit</button>
+            </form>
+        </ul>
+    </div>
+    <div class="">
+      <h1>Your Orders</h1>
+      <?php
+
+
+
+       ?>
+    </div>
+</body>
+
+</html>
