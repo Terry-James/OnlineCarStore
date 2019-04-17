641040d2d255d331cc2b1be9cedbd0705ebe459f
Terry-James <37746020+Terry-James@users.noreply.github.com>
2019-04-16 19:34:35 -0500

    added sessions to search

--- phpFiles/search.php
+++ phpFiles/search.php
@@ -1,14 +1,15 @@
 <?php
+session_start(); // most be first to start a session
+if(!isset($_SESSION['email'])){
+    header("Location: index.html"); // return to login if email is not set
+}
 
 $db_host = 'localhost';
 $db_username = 'root';
 $db_pass = '';
 $db_name = 'carstoredata';
 
-
-
 $db = new mysqli($db_host, $db_username, $db_pass, $db_name) or die("Can't connect to MySQL Server");
-
 
 $dataArray = array();
 $userSearch = $_REQUEST['q'];
